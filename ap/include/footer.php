808e440762df6d00a5022db37444971b08013150
lilpopsioo <104172962+lilpopsioo@users.noreply.github.com>
2022-05-12 13:14:37 +0200

    Delete ap directory

--- ap/include/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-<h2><a href="index.php">Accueil</a></h2>
-    <h3>Réservation de salles - Hugo VINCENT & Florentin HANNOIR</h3>
-    <h2><a href="https://gitlab.com/lilpopsio">GitLab</a></h2>
-</footer>
